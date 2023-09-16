895d81e9c13d50c1da154134a799ced422304977
Yohan-Launay <yohandeveloppe@gmail.com>
2023-09-16 16:00:23 +0200

    End game

--- 02_Rock_Paper_Scissors/scripts.js
+++ 02_Rock_Paper_Scissors/scripts.js
@@ -1,26 +1,66 @@
 let arrayGame = ["Paper", "Rock", "Scissors"];
-let paperValue = 0;
-let rockValue = 1;
-let scissorsValue = 2;
+let bot = 0
+let humain = 0;
+
+let pointBot = document.getElementById("bot");
+let pointHumain = document.getElementById("humain");
+let gameBot = document.getElementById("gameBot");
+let gameHumain = document.getElementById("gameHumain");
 
 // The paper win against the rock
 function paper() {
-    let randomGame = Math.floor(Math.random() * arrayGame.length.valueOf());
-    console.log("You play the paper" + scissorsValue + " " + randomGame);
-    if (scissorsValue === randomGame) {
+    let randomGame = Math.floor(Math.random() * arrayGame.length);
+
+    gameHumain.innerText = "Paper !"
+    gameBot.innerText = arrayGame[randomGame];
+    if (randomGame === 0) {
         console.log("Egal !")
+    } else if (randomGame === 1){
+        console.log("Gagné !")
+        humain++;
+        pointHumain.innerText = humain;
     } else {
-        console.log("Diff")
+        console.log('Perdu !');
+        bot++;
+        pointBot.innerText = bot;
     }
 }
 
 // The rock win against the scissors
 function rock() {
-    console.log("rock");
+    let randomGame = Math.floor(Math.random() * arrayGame.length);
 
+    gameHumain.innerText = "Rock !"
+    gameBot.innerText = arrayGame[randomGame];
+    // Si le random est égal au nombre de rock, c'est égal !
+    if (randomGame === 1) {
+        console.log("Egal !")
+    } else if (randomGame === 2){
+        console.log("Gagné !")
+        humain++;
+        pointHumain.innerText = humain;
+    } else {
+        console.log('Perdu !')
+        bot++;
+        pointBot.innerText = bot;
+    }
 }
 
 // The scissors win against the paper
 function  scissors() {
-    console.log("scissors");
+    let randomGame = Math.floor(Math.random() * arrayGame.length);
+
+    gameBot.innerText = arrayGame[randomGame];
+    gameHumain.innerText = "Scissors !"
+    if (randomGame === 2) {
+        console.log("Egal !")
+    } else if (randomGame === 0){
+        console.log("Gagné !");
+        humain++;
+        pointHumain.innerText = humain;
+    } else {
+        console.log('Perdu !')
+        bot++;
+        pointBot.innerText = bot;
+    }
 }
